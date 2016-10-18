112a055004b5b04f02a11c55cc34eabc4acf2f96
Javier Diaz <javiercoree@gmail.com>
2016-10-18 15:20:05 -0300

    Edited: Favicon; Added: Scrolleable menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 retinajs();
+$(document).ready(function () {
+  $('.scroll a[href^="#"]').click(function (e) {
+    e.preventDefault();
+    $(window).stop(true).scrollTo(this.hash, { duration: 500, interrupt: true });
+  });
+});
